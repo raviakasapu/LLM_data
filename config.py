c400ec3acae3e9471678473a6aa518c25d8ee9dd
Ravi Akasapu <raviakasapu@gmail.com>
2024-02-11 19:43:56 -0500

    rebase

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+OPENAI_API_KEY = "add your OpenAI key"
+HUGGINGFACEHUB_API_TOKEN = "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"
+#export TAVILY_API_KEY= 
+
+def set_environment():
+    variable_dict = globals().items()
+    for key, value in variable_dict:
+        if "API" in key or "ID" in key:
+            os.environ[key] = value
